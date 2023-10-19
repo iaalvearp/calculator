0964ac6a92e352b2ec05543604fede28a9b6fe63
Ali AlPes <iaalvearp@gmail.com>
2023-10-18 22:19:30 -0500

    Adding delete button

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,13 @@
 const btnInputs = document.querySelectorAll("input");
-const btns = document.getElementById("btns");
 const screenInput = document.getElementById("screenInput");
 const screenOutput = document.getElementById("screenOutput");
 
-btns.addEventListener("click", () => {
-  console.log(btnInputs.forEach((btn) => btn));
+btnInputs.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.value === "=") screenOutput.textContent = eval(screenInput.value);
+    else if (btn.value === "C") screenInput.textContent = 0;
+    else if (btn.id === "delBtn")
+      screenInput.textContent = screenInput.textContent.slice(0, -1);
+    else screenInput.textContent += btn.value;
+  });
 });
