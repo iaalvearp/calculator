14c36c001efa9c8e87f1df587e6ad838f0ee8022
Ali AlPes <iaalvearp@gmail.com>
2023-10-18 23:21:52 -0500

    Show total bug fixed

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,15 @@ const screenOutput = document.getElementById("screenOutput");
 
 btnInputs.forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (btn.value === "=") screenOutput.textContent = eval(screenInput.value);
-    else if (btn.value === "C") screenInput.textContent = 0;
-    else if (btn.id === "delBtn")
+    if (btn.value === "=") {
+      screenOutput.textContent = eval(screenInput.textContent);
+    } else if (btn.value === "C") {
+      screenOutput.textContent = "0";
+      screenInput.textContent = "";
+    } else if (btn.id === "delBtn") {
       screenInput.textContent = screenInput.textContent.slice(0, -1);
-    else screenInput.textContent += btn.value;
+    } else {
+      screenInput.textContent += btn.value;
+    }
   });
 });
